d1204bd2f613dc011a757571777e7ba7bc45fa2e
Xi Yangyang <stefaniexyy@hotmail.com>
2020-05-03 00:45:02 -0400

    20200502

--- src/js/onclick.js
+++ src/js/onclick.js
@@ -4,3 +4,9 @@ function click_like(){
     like_num=parseInt(like_num)+1
     document.getElementById("like_count").innerHTML=like_num+' like'
 }
+
+function login(){
+    var login_Obj=new XMLHttpRequest()
+    login_Obj.open("POST")
+    
+}
