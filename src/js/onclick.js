404f0a24290650a5f932e68b13e21427adef80b9
Xi Yangyang <stefaniexyy@hotmail.com>
2020-05-05 01:13:25 -0400

    update pixel on frontpage

--- src/js/onclick.js
+++ src/js/onclick.js
@@ -2,6 +2,8 @@ function click_like(){
     var like_num=/\d+/.exec(document.getElementById("like_count").innerText)
     document.getElementById("like_count")
     like_num=parseInt(like_num)+1
+    var pix=document.body.clientWidth
+    window.alert(pix)
     document.getElementById("like_count").innerHTML=like_num+' like'
 }
 
