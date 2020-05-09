d0811de93c22817c38467ee4d124e873873d5772
Xi Yangyang <stefaniexyy@hotmail.com>
2020-05-09 00:03:12 -0400

    sync 20200509

---
+++ src/js/chage_pixel.js
@@ -0,0 +1,14 @@
+window.onload=function chage_pixel(){
+    var curr_screen_width=document.body.clientWidth
+    if(curr_screen_width>1519){
+        curr_screen_width=(curr_screen_width-1519)/2
+    }else{
+        curr_screen_width=0
+    }
+    var title=document.getElementById("head");
+    var content=document.getElementById("box_content");
+    var tail=document.getElementById("tail");
+    title.style.marginLeft=curr_screen_width;
+    content.style.marginLeft=curr_screen_width;
+    tail.style.marginLeft=curr_screen_width;
+}
--- src/js/onclick.js
+++ src/js/onclick.js
@@ -7,8 +7,40 @@ function click_like(){
     document.getElementById("like_count").innerHTML=like_num+' like'
 }
 
-function login(){
-    var login_Obj=new XMLHttpRequest()
-    login_Obj.open("POST")
-    
+function loadXMLDoc(url)
+{
+xmlhttp=null;
+if (window.XMLHttpRequest)
+  {// code for Firefox, Opera, IE7, etc.
+  xmlhttp=new XMLHttpRequest();
+  }
+else if (window.ActiveXObject)
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+if (xmlhttp!=null)
+  {
+  xmlhttp.onreadystatechange=state_Change;
+  xmlhttp.open("GET",url,true);
+  xmlhttp.send(null);
+  }
+else
+  {
+  alert("Your browser does not support XMLHTTP.");
+  }
+}
+
+function state_Change()
+{
+if (xmlhttp.readyState==4)
+  {// 4 = "loaded"
+  if (xmlhttp.status==200)
+    {// 200 = "OK"
+    document.getElementById('T1').innerHTML=xmlhttp.responseText;
+    }
+  else
+    {
+    alert("Problem retrieving data:" + xmlhttp.statusText);
+    }
+  }
 }
